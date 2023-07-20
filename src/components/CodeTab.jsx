0ff04386134d779ca312694d4710e7eafe584bca
Jean-Yves <jy.babiarczyk@gmail.com>
2023-07-20 09:52:43 +0200

    first commit

---
+++ src/components/CodeTab.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function CodeTab() {
+  return (
+    <textarea 
+    spellCheck="false"
+    className="bg-zinc-900 text-slate-200 text-xl p-8 block w-full h-full focus:outline-none resize-none" 
+    >
+
+    </textarea>
+  )
+}
+
